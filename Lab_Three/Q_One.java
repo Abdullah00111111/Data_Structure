d047dd5e64b8a1abedaa679c0c58790e19b7d4a9
AbdullahMoammar <111189811+Abdullah00111111@users.noreply.github.com>
2023-01-03 23:23:40 +0300

    Add files via upload

---
+++ Lab_Three/Q_One.java
@@ -0,0 +1,12 @@
+package Lab_Three;
+
+public class Q_One {
+    public static void main(String[] args) {
+        
+        int[] arr = {1,2,3,4,5,6,7};
+        int[] arr2 = new int[7];
+        
+        for(int i = 0; i < arr2.length; i++)
+            arr2[i] = arr[i];
+    }
+}
