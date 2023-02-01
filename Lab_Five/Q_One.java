1b600c3fbcb1e9a271837f17f4e1aaf4e31073af
AbdullahMoammar <111189811+Abdullah00111111@users.noreply.github.com>
2023-02-01 04:46:22 +0300

    Add files via upload

---
+++ Lab_Five/Q_One.java
@@ -0,0 +1,13 @@
+package Lab_Five;
+
+public class Q_One {
+    public void addFirst(E e){
+        if(isEmpty()){
+            tail = new Node<>(e, tail.null);
+            tail.setNext(tail);
+        }
+        else
+            tail.next = new Node<>(e, tail.getNext());
+        size++;
+    }
+}
---
+++ Lab_Five/Q_Six.java
@@ -0,0 +1,15 @@
+package Lab_Five;
+
+public class Q_Six {
+    public void clone(CircularlyLinkedList list){
+        if(!isEmpty()){
+            Node<E> n = tail;
+            list.addFirst(n.element);
+            while(n.next != tail){
+                
+                n = n.next;
+                list.addFirst(n.element);
+            }
+        }
+    }
+}
